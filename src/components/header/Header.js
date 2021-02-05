2c1b99c12e3f8b37125727d8cff15d002dea562e
ryanepshortt <r.shortt98@gmail.com>
2021-02-05 03:06:40 -0500

    Updating site

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,15 +1,62 @@
-import React from 'react';
+import React,{useState, useEffect, useRef} from 'react';
 import './Header.css';
+
+function useOutsideAlerter(ref, setDrop) {
+    useEffect(() => {
+        /**
+         * Alert if clicked on outside of element
+         */
+        function handleClickOutside(event) {
+            if (ref.current && !ref.current.contains(event.target)) {
+                setDrop(false);
+            }
+        }
+
+        // Bind the event listener
+        document.addEventListener("mousedown", handleClickOutside);
+        return () => {
+            // Unbind the event listener on clean up
+            document.removeEventListener("mousedown", handleClickOutside);
+        };
+    }, [ref]);
+}
+
+
+
 function Header({showData, setShowData}) {
+    let [drop,setDrop] = useState(false);
+
+    const wrapperRef = useRef(null);
+    useOutsideAlerter(wrapperRef, setDrop);
+
     return (
         <div className='uk-width-1-1 uk-flex uk-flex-middle header-wrapper'>
             <div className='uk-flex-none'>
                 <span className='uk-text-large uk-text-bold uk-padding'>Ryan Shortt</span>
             </div>
-            <div className='uk-flex-auto'>
-                <span className={`${showData === 'Contact' ? 'selected' : 'nav-link'} uk-button uk-text-bold uk-float-right uk-margin-medium-right`} onClick={() => setShowData('Contact')}>Contact</span>
-                <span className={`${showData === 'About Me' ? 'selected' : 'nav-link'} uk-button uk-text-bold uk-float-right uk-margin-medium-right`} onClick={() => setShowData('About Me')}>About Me</span>
-                <span className={`${showData === 'Projects' ? 'selected' : 'nav-link'} uk-button uk-text-bold uk-float-right uk-margin-medium-right`} onClick={() => setShowData('Projects')}>Projects</span>
+            <div className='uk-flex-auto '>
+
+
+                <span className={`${showData === 'Contact' ? 'selected' : 'nav-link'} uk-button uk-text-bold uk-float-right uk-margin-right uk-visible@s`}
+                      onClick={() => setShowData('Contact')}>Contact</span>
+                <span className={`${showData === 'About Me' ? 'selected' : 'nav-link'} uk-button uk-text-bold uk-float-right uk-visible@s`}
+                      onClick={() => setShowData('About Me')}>About Me</span>
+                <span className={`${showData === 'Projects' ? 'selected' : 'nav-link'} uk-button uk-text-bold uk-float-right uk-margin-remove-left uk-visible@s`}
+                      onClick={() => setShowData('Projects')}>Projects</span>
+
+
+                <button type='button' data-uk-icon='icon: table' className='table-icon uk-icon uk-button uk-text-bold uk-float-right uk-margin-right uk-hidden@s'
+                        onClick={() => {setDrop(!drop)}}
+                />
+
+                <div ref={wrapperRef} id='drop' className={`uk-hidden@s custom-dropdown ${drop ? '' : 'uk-hidden'}`}>
+                    <div className="uk-text-center uk-inline">
+                        <div className={`${showData === 'Projects' ? 'dd-selected' : 'dd-option'}`} onClick={() => {setShowData('Projects'); setDrop(false)}}>Projects</div>
+                        <div className={`${showData === 'About Me' ? 'dd-selected' : 'dd-option'} uk-padding uk-padding-remove-horizontal`} onClick={() => {setShowData('About Me'); setDrop(false)}}>About Me</div>
+                        <div className={`${showData === 'Contact' ? 'dd-selected' : 'dd-option'}`}  onClick={() => {setShowData('Contact'); setDrop(false)}}>Contact</div>
+                    </div>
+                </div>
+
             </div>
         </div>
     );
--- src/pages/projects/Projects.js
+++ src/pages/projects/Projects.js
@@ -1,30 +1,36 @@
 import React from 'react';
-import logo from '../../../public/logo512.png';
+import smartHome from '../../assets/smart-home.jpeg';
+import boogie from '../../assets/Boogie.png';
+
+import './Projects.css'
 function Projects() {
     return (
-        <div>
+        <div >
             <div className="uk-child-width-1-2@m" data-uk-grid>
                 <div>
                     <div className="uk-card uk-card-default">
-                        <div className="uk-card-media-top">
-                            <img src={logo} alt=""/>
+                        <div className="uk-card-media-top media-card">
+                            <img src={smartHome}  alt=""/>
                         </div>
                         <div className="uk-card-body">
-                            <h3 className="uk-card-title">Media Top</h3>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-                                incididunt.</p>
+                            <h3 className="uk-card-title">Smart Home Microsite</h3>
+                            <p>Designed and implemented a smart home microsite for a McMaster-sponsored company.
+                                The application administers distributed control over hardware nodes using
+                                NodeJs, React, Redux, and Formik. In addition to creating the UI,
+                                I was involved in developing the REST API layer using Express and Postgres.</p>
                         </div>
                     </div>
                 </div>
                 <div>
                     <div className="uk-card uk-card-default">
-                        <div className="uk-card-body">
-                            <h3 className="uk-card-title">Media Bottom</h3>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-                                incididunt.</p>
+                        <div className="uk-card-media-top media-card">
+                            <img src={boogie}  alt=""/>
                         </div>
-                        <div className="uk-card-media-bottom">
-                            <img src='../../../public/logo512.png' alt=""/>
+                        <div className="uk-card-body">
+                            <h3 className="uk-card-title">Music-Themed Social Media Site</h3>
+                            <p>Developed a personal music-themed website where users could advertise their own skills,
+                                or any upcoming events or shows. The website was designed for mobile as well as desktop,
+                                and was built using HTML, CSS, JavaScript, JQuery and the Google Maps API.</p>
                         </div>
                     </div>
                 </div>
