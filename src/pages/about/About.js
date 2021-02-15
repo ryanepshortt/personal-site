98950a056bfd7c4507fb403aa499d77eaf67df51
ryanepshortt <r.shortt98@gmail.com>
2021-02-15 15:54:03 -0500

    hiding div until image is loaded

--- src/pages/about/About.js
+++ src/pages/about/About.js
@@ -35,9 +35,9 @@ function About() {
             <h2 style={{color: theme.cardTitle}}> Education and Employment</h2>
 
             <div className='custom-margin' >
-                <div className="uk-inline about-card uk-animation-fade"  style={{color: theme.overlayText}}>
+                <div className="uk-inline about-card"  style={{color: theme.overlayText}}>
                     <img  src={Mac} alt=""  className={`smooth-image image-${imageLoaded ? 'visible' :  'hidden'}`} onLoad={()=> setImageLoaded(true)}/>
-                    <div className={`uk-overlay uk-position-bottom overlay-plate ${imageLoaded ? '' : 'uk-hidden'} ${dark ? 'uk-overlay-primary' : 'uk-overlay-default'}`}>
+                    <div className={`uk-overlay uk-position-bottom overlay-plate ${imageLoaded ? 'image-visible' : 'uk-hidden'} ${dark ? 'uk-overlay-primary' : 'uk-overlay-default'}`}>
                         <p className='uk-margin-remove-bottom'><span className='uk-text-bold'>McMaster University</span> - Computer Engineering <span className='uk-visible@s'>Co-Op (2016-2021)</span></p>
                         <p className='uk-margin-small-top'>Dean's List: 3A, 3B, 4A</p>
                     </div>
@@ -47,7 +47,7 @@ function About() {
             <div className="uk-child-width-1-2@m uk-text-center uk-grid-match" data-uk-grid>
 
                 <div>
-                    <div className="uk-inline about-card uk-animation-fade" style={{color: theme.overlayText}}>
+                    <div className="uk-inline about-card" style={{color: theme.overlayText}}>
                         <img onLoad={()=> setImageLoaded(true)} className='work-img' src={ciena} alt=""/>
                         <div className={`uk-overlay  uk-position-bottom overlay-plate ${dark ? 'uk-overlay-primary' : 'uk-overlay-default'}`}>
                             <p className='uk-margin-remove-bottom'><span className='uk-text-bold'>Ciena</span> - Automation Tools Developer (Co-Op)</p>
